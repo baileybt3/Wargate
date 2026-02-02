c8f37f2439dfec2163041c4c77ed573ffd69b19a
baileybt3 <139825786+baileybt3@users.noreply.github.com>
2026-02-02 18:39:03 -0500

    feat: add main menu flow with scenecontroller

--- Assets/Scripts/Bootstrapper.cs
+++ Assets/Scripts/Bootstrapper.cs
@@ -5,16 +5,31 @@ using Unity.Netcode;
 public class Bootstrapper : MonoBehaviour
 {
     [SerializeField] private string mainMenuSceneName = "WargateMainMenu";
+    [SerializeField] private SceneController sceneController;
 
     private void Awake()
     {
-        if (NetworkManager.Singleton != null && NetworkManager.Singleton.gameObject != gameObject)
+        // if NetworkManager already exists, don't want another bootstrap stack
+        if (NetworkManager.Singleton != null)
         {
-            Destroy(gameObject);
+            // Make NetworkManager persists
+            DontDestroyOnLoad(NetworkManager.Singleton.gameObject);
+
+            // If SceneController is on this scene, persist it
+            if (sceneController != null)
+                DontDestroyOnLoad(sceneController.gameObject);
+
+            // This bootstrapper object can be destroyed if it's not the NetworkManager object
+            if (gameObject != NetworkManager.Singleton.gameObject)
+                Destroy(gameObject);
+
             return;
         }
 
         DontDestroyOnLoad(gameObject);
+
+        if (sceneController != null)
+            DontDestroyOnLoad(sceneController.gameObject);
     }
 
     private void Start()
